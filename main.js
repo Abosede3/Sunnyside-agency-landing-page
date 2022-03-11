978670e8b7772ae508593e6ec85f5f233cec96f0
Abosede Awoniyi <awoniy.abosede@gmail.com>
2022-03-11 12:31:42 +0100

    hamburger functionality

---
+++ main.js
@@ -0,0 +1,16 @@
+const mainMenu = document.querySelector('.mainMenu');
+const closeMenu = document.querySelector('.close-menu');
+const openMenu = document.querySelector('.hamburger');
+
+
+openMenu.addEventListener('click', show);
+closeMenu.addEventListener('click', close);
+
+function show(){
+  mainMenu.style.display = 'flex';
+  mainMenu.style.top = '0';
+}
+
+function close(){
+  mainMenu.style.top = '-100%';
+}
